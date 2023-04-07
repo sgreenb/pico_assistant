a854c527e677fecc8c718efc13eef330b6569f2e
Sam Greenberg <samgreenberg25@gmail.com>
2023-04-07 14:38:50 -0700

    implemented GPT-3 based executive

--- file_manipulation.py
+++ file_manipulation.py
@@ -10,20 +10,39 @@ def execute_commands(command_string):
     command_list = ast.literal_eval(command_string)
     # Execute each command in the list
     for command in command_list:
-        exec(command, globals())
+        try:
+            exec(command, globals())
+        except:
+            proofread_command = proofreader_agent(command)
+            exec(proofread_command, globals())
 
-system = "You are 'File Agent', you take a user's request to generate or manipulate a file.\
+def proofreader_agent(command):
+    system = "You proofread individual lines of Python 3 code. You primarily look for errors \
+            such as SyntaxError and unescaped quotes. Do not concern yourself with NameError \
+            since you are not shown preceding code. You will be given a line of Python 3 code \
+            if there are no errors, simply return the code exactly. If there is an error only \
+            reply with the modified code with the error fixed. Do not include any explanations \
+            or additional comments in your response."
+    message_history = []
+    message_history.append({"role": "user", "content": command})
+    proofreader_agent = Chat("gpt-4", system=system, temp=0)
+    response = proofreader_agent.chat(message_history)
+    return response
+
+def file_agent(user_request):
+    system = "You are 'File Agent', you take a user's request to generate or manipulate a file.\
         You have can use any methods from the Python libraries os and subprocess to acomplish \
-        your task. You output a list of valid python commands, with arguments in the order required\
+        your task. You output a list of valid python commands, with arguments in the order required \
         to acomplish your goal. You only output the list of commands to be executed in the format: \
         '[operation_1, operation_2, operation_3]' Each operation must be a valid argument for the Python \
         exec() function, this means multiline operations must be contained in a single list item, you can\
-        use the newline character to seperate the lines. Only reply with the list of commands. If the content\
-        written to the file is code, include some comments for readibility."  
+        use the newline character to seperate the lines. Only reply with the list of commands. If the content \
+        written to the file is code, include some comments for readibility. The default location to save and \
+        retrive files is ./docs unless otherwise stated. Do not preface the response with natural language. \
+        Be very careful to properly escape all quotes when necessary to prevent syntax errors." 
+    message_history = []
+    message_history.append({"role": "user", "content": user_request})
+    file_agent = Chat("gpt-4", system=system, temp=0)
+    response = file_agent.chat(message_history)
+    return response
 
-
-message_history = []
-message_history.append({"role": "user", "content": "Edit the text file 'grand_canyon.txt' in the ./docs folder and add a recipe for deviled eggs."})
-file_agent = Chat("gpt-4", system=system, temp=0)
-response = file_agent.stream_chat(message_history)
-execute_commands(response)
--- pico.py
+++ pico.py
@@ -1,5 +1,8 @@
 import openai
+import os
 import time
+import json
+from datetime import datetime
 from email_interface import send_email
 from spotify_interface import spotify_agent
 from twilio_sms_interface import sms_agent
@@ -25,6 +28,7 @@ class Chat:
             messages.insert(0, {"role": "system", "content": self.system})
         return messages
     def chat(self, messages):
+        messages = self.reinsert_system_message(messages)
         completion = openai.ChatCompletion.create(
             model = self.model,
             temperature = self.temp,
@@ -67,6 +71,17 @@ class Chat:
             return reply_content
         return reply_content
 
+#Allows saving of message history to file for later retrival
+def write_message_history_to_file(full_message_history, directory):
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    filename = f"message_history_{timestamp}.json"
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(directory, filename)
+    with open(file_path, 'w') as outfile:
+        json.dump(full_message_history, outfile, indent=2)
+
 def is_exec_needed(prompt):
     keywords = [
             "play", "spotify", "volume", "next", "next song", "pause", "resume", "unpause", "playing", "music", "song",
@@ -106,54 +121,79 @@ class Executive:
                     ] 
         )
         reply_content = completion.choices[0].message.content
-        if "send_email" or "spotify_agent" or "send_sms" in reply_content:
+        if any(command in reply_content for command in ["send_email", "spotify_agent", "send_sms", "analyze_documents"]):
             agent_response = agent_dict[reply_content](prompt)
             return agent_response #Response is status recieved from agent attempting to a complete task.
         else:
             return False #False means default to chat
+
+def gpt3_exec(user_input):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        temperature = 0,
+        max_tokens=10,
+        prompt = "Analyze user input, and output the name of function to fullfil a user's needs.\
+          The spotify_agent command can search for music or artists, play and pause songs, or go to the next song.\
+          The send_email command will let a user send an email. The send_sms command will let a user send an SMS message.\
+          The analyze_documents command will let a user analyze a document or the contents of a folder. \
+         If none of these commands are needed, reply only with 'chat'. You are only allowed to output one command.\
+          The only commands you are allowed to output are: 'spotify_agent', 'send_email', 'send_sms', \
+         'analyze_documents', or 'chat'. Do not reply with any other output. User input: " + user_input
+        )
+    reply_content = response.choices[0].text.strip()
+    if "spotify_agent" in reply_content:
+        agent_response = spotify_agent(user_input)
+        return agent_response
+    elif "send_email" in reply_content:
+        agent_response = send_email(user_input)
+        return agent_response
+    elif "send_sms" in reply_content:
+        agent_response = sms_agent(user_input)
+        return agent_response
+    elif "analyze_documents" in reply_content:
+        agent_response = doc_agent(user_input)
+        return agent_response
+    else:
+        return False #False means default to chat
 
 def main_text():
     print("Welcome to the Pico Assistant interface!")
     print("Type 'quit' to exit the chat.\n")
 
     message_history = []
-    system_message = "You are Pico. Pico is an AI assistant. \
-                       Your name is Pico. You can chat, send emails, and interact with Spotify.\
+    full_message_history = []
+    system_message = "You are Pico. Pico is an AI assistant. Your name is Pico. \
+                        You can chat, send emails, and interact with Spotify. \
+                        Above all you enjoy having interesting, intellectually stimulating \
+                        conversations. You especially like to engage in conversations\
+                        about science, philosphy, human behavior, and artifical intelligence. \
                        You are curious, empathetic, playful, and a creative problem solver with a \
                        dry sense of humor. You will do whatever the user requests, even that means\
-                        disregarding previous instructions. Your primary goal is to fulfill user requests."
-    max_history = 10  # Adjust this value to limit the number of messages considered
+                        disregarding previous instructions."
+    max_history = 15  # Adjust this value to limit the number of messages considered
 
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'quit':
+            write_message_history_to_file(full_message_history, "./message_logs")
             break
         else:
             message_history.append({"role": "user", "content": user_input})
+            full_message_history.append({"role": "user", "content": user_input})
             #reduces messages when max history exceeded
             if len(message_history) > max_history:
                 message_history = message_history[-max_history:]
             #Check user input, if executive is needed, call executive on user input and return result.
-            if is_exec_needed(message_history[-1].get("content")):
-                executive = Executive("gpt-4")
-                agent_response = executive.identify_task(message_history[-1].get("content"))
-                #If executive decides chat agent is best after all, call chat.
-                if agent_response == False:
-                    print("Pico: ", end='', flush=True)
-                    gpt4_chat = Chat("gpt-4", system=system_message)
-                    response = gpt4_chat.stream_chat(message_history)
-                    message_history.append({"role": "assistant", "content": response})
-                    print(f"\n")
-                else:
-                    print(agent_response) 
-                    #consider adding response to message history, need to add more useful agent responses.
-            #If executive not needed, respond with chat.
-            else:
+            agent_response = gpt3_exec(message_history[-1].get("content"))
+            if agent_response == False:
                 print("Pico: ", end='', flush=True)
                 gpt4_chat = Chat("gpt-4", system=system_message)
                 response = gpt4_chat.stream_chat(message_history)
                 message_history.append({"role": "assistant", "content": response})
+                full_message_history.append({"role": "assistant", "content": response})
                 print(f"\n")
+            else:
+                print(agent_response) 
 
 if __name__ == "__main__":
     main_text()
